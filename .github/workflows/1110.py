17d8a3a96b3c1b9f8299d3290069e5d1e2be4521
cashew09 <99326014+cashew09@users.noreply.github.com>
2022-02-09 19:22:12 +0900

    Create 1110.py

---
+++ .github/workflows/1110.py
@@ -0,0 +1,13 @@
+n = num = int(input())
+count = 0
+
+while True:
+	ten = n //10
+	one = n %10
+	total = ten + one
+	count +=1
+	n = int(str(one)+str(total%10))
+	if n == num:
+		break
+
+print(count)
